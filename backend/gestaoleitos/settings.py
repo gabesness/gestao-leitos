99d3d601848e82bef45eba391ad82e9209a69754
gabesness <46109590+gabesness@users.noreply.github.com>
2024-05-30 18:28:16 -0300

    Permissao CORS

--- backend/gestaoleitos/settings.py
+++ backend/gestaoleitos/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_gestao',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'gestaoleitos.urls'
