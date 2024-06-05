2503da856ae9fa01f9636ece7f3b346ae3858b75
gabesness <46109590+gabesness@users.noreply.github.com>
2024-06-05 18:35:20 -0300

    Tratamento de erros das rotas; rota de login concluida

--- backend/app_gestao/views.py
+++ backend/app_gestao/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 #from django.middleware.csrf import get_token
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from django.core import serializers
+from django.forms.models import model_to_dict
 
 # Create your views here.
 
@@ -13,15 +15,26 @@ from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 @csrf_exempt
 def fazer_login(request):
-    username = request.POST.get("username", '')
-    password = request.POST.get("password", '')
-    user = authenticate(request, username=username, password=password)
+    try:
+        username = request.POST.get("username", '')
+        password = request.POST.get("password", '')
+        user = authenticate(request, username=username, password=password)
 
-    if user is not None:
-        login(request, user)
-        return JsonResponse({"mensagem": "logou"})
-    else:
-        return JsonResponse({"mensagem": "erro"})
+        if user is not None:
+            login(request, user)
+            data = {
+                'id': user.id,
+                'username': user.username,
+                'nome': user.first_name,
+                'sobrenome': user.last_name,
+                'email': user.email,
+                'groups': list(user.groups.values_list('name', flat=True)),
+            }
+            return JsonResponse(data)
+        else:
+            return JsonResponse({"erro": "usuario/senha incorretos"})
+    except Exception as e:
+        return JsonResponse({"erro": str(e)})
 
 def esqueceu_senha(request):
     email = request.POST.get('email', '')
@@ -50,9 +63,11 @@ def dashboard(request):
     pass
 
 def minha_conta(request, id):
-    user = User.objects.filter(id=id).values()
-    print(user)
-    return JsonResponse(list(user), safe=False)
+    try:
+        user = User.objects.filter(id=id).values()
+        return JsonResponse(list(user), safe=False)
+    except Exception as e:
+        return JsonResponse({"erro": str(e)})
 
 def lista_pacientes(request):
     pacientes = Paciente.objects.all().values()
@@ -62,17 +77,26 @@ def lista_pacientes(request):
 def criar_paciente(request):
     nome = request.POST.get('nome', '')
     prontuario = request.POST.get('prontuario', '')
-    #print(nome, prontuario)
-    p = Paciente(prontuario=prontuario, nome=nome, estagio_atual="Paciente cadastrado")
-    p.save()
-    ok = {"mensagem": "paciente criado!!!!!"}
-    return JsonResponse(ok)
+    try:
+        if Paciente.objects.get(prontuario=prontuario).exists():
+            p = Paciente(prontuario=prontuario, nome=nome, estagio_atual="Paciente cadastrado")
+            p.save()
+            return JsonResponse({"OK": "paciente criado com sucesso!"})
+        else:
+            return JsonResponse({"ERRO": "já existe paciente cadastrado com este prontuário"})
+    except Exception as e:
+        return JsonResponse({"erro": str(e)})
 
 def alterar_dados_do_usuario(request, id):
-    nome = request.PUT.get('first_name', '')
-    sobrenome = request.PUT.get('last_name', '')
-    email = request.PUT.get('email', '')
-    if nome: u.first_name = nome
-    if sobrenome: u.last_name = sobrenome
-    if email: u.email = email
-    u = User.objects.get(id=id)
+    try:
+        nome = request.PUT.get('first_name', '')
+        sobrenome = request.PUT.get('last_name', '')
+        email = request.PUT.get('email', '')
+        if nome: u.first_name = nome
+        if sobrenome: u.last_name = sobrenome
+        if email: u.email = email
+        u = User.objects.get(id=id)
+        return JsonResponse({"OK": "dados alterados com sucesso!"})
+    except Exception as e:
+        return JsonResponse({"erro": str(e)})
+
