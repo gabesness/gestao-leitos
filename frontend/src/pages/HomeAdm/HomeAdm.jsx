0da03c805334222780acb59ff4559c1e10b0b523
João Victor <joaovictor@MacBook-Joao-Victor.local>
2024-08-17 16:17:48 -0300

    cadastro usuario frontend

--- frontend/src/pages/HomeAdm/HomeAdm.jsx
+++ frontend/src/pages/HomeAdm/HomeAdm.jsx
@@ -36,7 +36,115 @@ import Pagination from '../../components/Pagination/Pagination';
 import UsuarioCard from '../../components/Cards/UsuarioCard';
 import HistoricoCard from '../../components/Cards/HistoricoCard';
 import CabecalhoUsuario from '../../components/Ficha/CabecalhoUsuario';
+import { ToastContainer, toast } from 'react-toastify';
+import { AxiosURL } from '../../axios/Config';
 
+
+function ModalNovoUsuario({ isOpen, onClose }) {
+  const [selectedCargo, setSelectedCargo] = useState('Selecione o Cargo');
+
+  const [formData, setFormData] = useState({
+    nome: '',
+    sobrenome: '',
+    email: '',
+    username: '',
+    senha: ''
+  });
+
+  const handleSelectCargo = (cargo) => {
+    setSelectedCargo(cargo);
+  };
+
+  const handleClose = () => {
+    if (isOpen) {
+      onClose();
+    }
+  };
+
+ const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.id]: event.target.value
+    });
+  };
+
+  const cargoMap = {
+    'Médico': 1,
+    'Farmácia': 2,
+    'Administrador': 3,
+    'Regulação': 4,
+    'Recepcionista': 5,
+  };
+
+  const CriarUsuario = async (event) => {
+    event.preventDefault();
+    
+    try {
+      const response = await axios.patch(`${AxiosURL}/usuarios/criar_usuario/`, {
+        username: formData.username,
+        password: formData.senha,
+        first_name: formData.nome,
+        last_name: formData.sobrenome,
+        email: formData.email,
+        groups: [cargoMap[selectedCargo]],
+      });
+      if (response.status === 204) {
+      toast.success(response.data.message);
+
+      }
+
+    } catch (error) {
+      console.error('Erro ao criar usuário:', error);
+      toast.error(error.response.data.erro);
+    }
+  };
+
+  return (
+    <MDBModal open={isOpen} onClose={handleClose} tabIndex='-1' appendToBody>
+      <MDBModalDialog>
+        <MDBModalContent>
+          <MDBModalHeader>
+            <MDBModalTitle>Novo Usuário</MDBModalTitle>
+            <MDBBtn className='btn-close' color='none' onClick={handleClose}></MDBBtn>
+          </MDBModalHeader>
+          <MDBModalBody>
+            <div style={{ marginBottom: '15px' }}>
+              <MDBInput label="Nome" id="nome" type="text" onChange={handleChange} />
+            </div>
+            <div style={{ marginBottom: '15px' }}>
+              <MDBInput label="Sobrenome" id="sobrenome" type="text" onChange={handleChange} />
+            </div>
+            <div style={{ marginBottom: '15px' }}>
+              <MDBInput label="Email" id="email" type="text" onChange={handleChange} />
+            </div>
+            <div style={{ marginBottom: '15px' }}>
+              <MDBInput label="Username" id="username" type="text" onChange={handleChange} />
+            </div>
+            <div style={{ marginBottom: '15px' }}>
+              <MDBInput label="Senha" id="senha" type="password" onChange={handleChange} />
+            </div>
+            <div style={{ marginBottom: '15px' }}>
+              <MDBDropdown>
+                <MDBDropdownToggle style={{ width: '100%' }}>{selectedCargo}</MDBDropdownToggle>
+                <MDBDropdownMenu style={{ width: '100%' }}>
+                <MDBDropdownItem link onClick={() => handleSelectCargo('Médico')}>Médico</MDBDropdownItem>
+                <MDBDropdownItem link onClick={() => handleSelectCargo('Farmácia')}>Farmácia</MDBDropdownItem>
+                <MDBDropdownItem link onClick={() => handleSelectCargo('Regulação')}>Regulação</MDBDropdownItem>
+                <MDBDropdownItem link onClick={() => handleSelectCargo('Recepcionista')}>Recepcionista</MDBDropdownItem>
+                <MDBDropdownItem link onClick={() => handleSelectCargo('Administrador')}>Administrador</MDBDropdownItem>
+                </MDBDropdownMenu>
+              </MDBDropdown>
+            </div>
+          </MDBModalBody>
+          <MDBModalFooter>
+            <MDBBtn onClick={CriarUsuario}>Criar</MDBBtn>
+          </MDBModalFooter>
+        </MDBModalContent>
+      </MDBModalDialog>
+      <ToastContainer />
+    </MDBModal>
+  );
+}
 
 function QuadroLista({ usuarios, activeTab, selectedUser, handleUserClick, setActiveTab }) {
   const [currentPage, setCurrentPage] = useState(1);
@@ -59,6 +167,10 @@ function QuadroLista({ usuarios, activeTab, selectedUser, handleUserClick, setAc
 
   const currentUsuarios = searchedUsuarios.slice(indexOfFirstPost, indexOfLastPost);
 
+  const [basicModal, setBasicModal] = useState(false);
+
+  const toggleOpen = () => setBasicModal(!basicModal);
+
   return (
     <MDBCol md='4'>
       <MDBCard
@@ -74,21 +186,39 @@ function QuadroLista({ usuarios, activeTab, selectedUser, handleUserClick, setAc
         }}
       >
         {/* Conteúdo */}
-        <MDBCardBody className="d-flex flex-column" style={{ flex: '1 1 auto', minHeight: '0' }}>
-          {/* Cabeçalho */}
-          <MDBInput
-            type="text"
-            label={
-              <div className="d-flex align-items-center">
-                <MDBIcon fas icon="search" className="me-2" />
-                Pesquisar
-              </div>
-            }
-            value={searchTerm}
-            onChange={handleSearchChange}
-            className="flex-grow-1 mb-3"
-            style={{ height: '24px' }}
-          />
+        <MDBCardBody
+          className="d-flex flex-column"
+          style={{ flex: '1 1 auto', minHeight: '0' }}
+        >          {/* Cabeçalho */}
+          <div className="d-flex align-items-center mb-2" style={{ marginTop: '0px' }}>
+            
+            <MDBInput
+              type="text"
+              label={
+                <div className="d-flex align-items-center">
+                  <MDBIcon fas icon="search" className="me-2" />
+                  Pesquisar
+                </div>
+              }
+              value={searchTerm}
+              onChange={handleSearchChange}
+              className="flex-grow-1"
+              style={{ height: '40px'}}
+            />
+            
+            <MDBBtn
+              onClick={toggleOpen}
+              className="ms-2 d-flex justify-content-center align-items-center btn-sm"
+              style={{
+                minWidth: '40px',
+                height: '40px',
+                margin: '0',
+              }}
+              color="primary"
+            >
+              <MDBIcon fas icon="plus" style={{ fontSize: '16px' }} />
+            </MDBBtn>
+          </div>
   
           {/* Listagem */}
           <MDBListGroup light style={{ flex: '1 1 auto', overflowY: 'auto' }}>
@@ -114,6 +244,7 @@ function QuadroLista({ usuarios, activeTab, selectedUser, handleUserClick, setAc
           )}
         </MDBCardBody>
       </MDBCard>
+      <ModalNovoUsuario isOpen={basicModal} onClose={toggleOpen} />
     </MDBCol>
   );
   
@@ -199,8 +330,7 @@ function HomeAdm() {
   return (
     <MDBContainer fluid className='p-1 background-radial-gradient overflow-hidden d-flex justify-content-center'  style={{ minHeight: '100vh' }}>
       <MDBCard className='my-5 bg-glass max-width-card' style={{ width: '100%', maxWidth: '1200px', borderRadius: '38px' }}>
-      <h2 style={{ marginTop: '10px', marginLeft: '10px', marginBottom: '-8px' }}>Gerenciamento de Usuários</h2>
-      <hr style={{ marginBottom: '10px' }} />
+      <h2 style={{ marginTop: '15px', marginLeft: '50px', marginBottom: '-22px' }}>Gerenciamento de Usuários</h2>
       <MDBCardBody className='p-5'>
           <MDBRow>
           <QuadroLista
